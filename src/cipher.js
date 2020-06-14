9bcb6f75ed6cfabe8f5a4b83ebb2c9362b76f4eb
Geraldien Villacura R <geraldinevillacuraruz@gmail.com>
2020-06-14 17:27:54 -0400

    Se agrega página principal, código javascript pa cifrado y descifrado.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,61 @@
 const cipher = {
-  // ...
+  encode: encodeMessage,
+  decode: decodeMessage
 };
+
+function encodeMessage(offset, string) {
+
+  let wordEncrypted = "";
+
+  let uppercaseMessage = string.toUpperCase();
+
+  offset = Number(offset);
+
+  for (let index = 0; index < uppercaseMessage.length; index++) {
+
+    let x = uppercaseMessage.charCodeAt(index);
+    let positionAscci = (x - 65 + offset) % 26 + 65;
+
+    if (x < 65 || x > 90) {
+      wordEncrypted = wordEncrypted + uppercaseMessage[index];
+      continue;
+    }
+    let encryptedLetter = String.fromCharCode(positionAscci);
+
+    wordEncrypted = wordEncrypted + encryptedLetter;
+
+  }
+
+  return wordEncrypted;
+
+}
+
+
+function decodeMessage(offset, string) {
+
+  let wordDecrypted = "";
+
+  let uppercaseMessage = string.toUpperCase();
+
+  offset = Number(offset);
+
+  for (let index = 0; index < uppercaseMessage.length; index++) {
+
+    let x = uppercaseMessage.charCodeAt(index);
+    let positionAscci = (x + 65 - offset) % 26 + 65;
+
+    if (x < 65 || x > 90) {
+      wordDecrypted = wordDecrypted + uppercaseMessage[index];
+      continue;
+    }
+    let letraEncriptada = String.fromCharCode(positionAscci);
+
+    wordDecrypted = wordDecrypted + letraEncriptada;
+
+  }
+
+  return wordDecrypted;
+
+}
 
 export default cipher;
